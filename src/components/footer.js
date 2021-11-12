48d05f210d84c59c908eee04d86ee80f9bad66fe
Maria Järvinen <n9jama00@students.oamk.fi>
2021-11-12 14:58:38 +0200

    kirjaudu alkua ja tyyli tiedostoa

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../style/style.css';
 
 export default function Footer() {
 
 
     return (
-        <footer>
+        <footer class='footer'>
             <div>
                 <p>&copy; ResComi</p>
             </div>
--- src/sivut/kirjaudu.js
+++ src/sivut/kirjaudu.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import '../style/style.css';
+
 
 export default function Kirjaudu() {
+  const [ktunnus, setKtunnus] = useState('');
+  const [salasana, setSalasana] = useState('');
+
+  function onPress(){
+
+  }
+
   return (
-    <div >
-      <p>kirjaudu sisään</p>
+    <div class='container kirjaudu'>
+        <div>
+          <div>
+            <h2>Kirjaudu sisään</h2>
+          </div>
+          <div>
+            <form onSubmit={onPress()}>
+
+              <div class='mb-3'>
+                <label class="form-label">Käyttäjätunnus</label>
+                <input type='text' class="form-control" name='ktunnus'
+                value= {ktunnus} placeholder='Käyttäjätunnus' 
+                onChange={e => setKtunnus(e.target.value)}/>
+              </div>
+
+              <div class='mb-3'>
+                <label class="form-label">Salasana</label>
+                <input type='password' class="form-control input" name='salasana' placeholder='Salasana' 
+                value={salasana} onChange={e => setSalasana(e.target.value)}/>
+              </div>
+              
+              <button class='btn btn-primary button'>Kirjaudu sisään</button>
+            </form>
+        </div>
+      </div>
     </div>
   );
 }
