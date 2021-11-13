b362ed4a591b78bc46a03e7d208411e0ab1413dd
Maria Järvinen <n9jama00@students.oamk.fi>
2021-11-13 16:46:09 +0200

    koti-sivun rankaa

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,9 +7,7 @@ export default function Footer() {
     return (
         <footer class='footer container-fluid'>
             <div class='row'>
-                <div class='col'>
                     <p>&copy; ResComi</p>
-                </div>
             </div>
             
         </footer>
--- src/sivut/koti.js
+++ src/sivut/koti.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {FaWarehouse, FaSearch } from 'react-icons/fa'; 
+import {MdPersonAdd } from 'react-icons/md';
+import {GiTireIron} from 'react-icons/gi';
+
 
 export default function Koti() {
+  const [haku, setHaku]= useState('');
+
   return (
-    <div>
-      <h1></h1>
-      <p>tässä kirjautuneen käyttäjän näkymä</p>
+    <div class='container-fluid'>
+      <div class='row'>
+        <div class='col'>
+        </div>
+
+        <div class='col-4 nappi'>
+          <p>Hae
+          <input class="form-control" name='haku'
+            value= {haku} placeholder='Rekisteri tai puh'/>
+            <FaSearch size={20}/></p>
+        </div>
+        <div class='col-4 nappi'>
+          <FaWarehouse size={40}/>
+          <p>Varasto</p>
+        </div>
+
+        <div class='col'>
+        </div>
+      </div>
+
+      <div class='row'>
+        <div class='col'>
+        </div>
+
+        <div class='col-4 nappi'>
+          <MdPersonAdd size={40} />
+          <p>Uusi Asiakas</p>
+        </div>
+
+        <div class='col-4 nappi'>
+          <GiTireIron size={40} />
+          <p>Valmiit työt</p>
+        </div>
+
+        <div class='col'>
+        </div>
+      </div>
+      
     </div>
   );
 }
