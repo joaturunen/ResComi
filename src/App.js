6b69a6f1a8678a98e82413271d8227303ef684c8
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2021-11-15 21:12:09 +0200

    Näyttää yhden asiakkaan tiedot

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Kirjaudu from './sivut/kirjaudu';
 import Koti from './sivut/koti';
 import UusiAsiakas from './sivut/uusiAsiakas';
 import Varasto from './sivut/varasto';
+import Asiakas from './sivut/Asiakas';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route path="/sivut/koti" element={<Koti />}/>
           <Route path="/sivut/uusiAsiakas" element={<UusiAsiakas/>}/>
           <Route path="/sivut/varasto" element={<Varasto/>}/>
+          <Route path="/sivut/Asiakas" element={<Asiakas/>}/>
         </Routes>
         <Footer/>
       </Router>
--- src/sivut/uusiAsiakas.js
+++ src/sivut/uusiAsiakas.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+
+const URL = "http://localhost:8888/rengashotelli/API/customer/create.php";
 
 export default function UusiAsiakas() {
+
   return (
     <div >
       <h1>lisää uusi asiakas</h1>
