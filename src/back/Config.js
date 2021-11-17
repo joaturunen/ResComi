85ffe09215581c6772c265a76b6517be00208ce5
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2021-11-17 10:08:25 +0200

    Luotu back-kansio. Sinne polut

---
+++ src/back/Config.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Config = () =>{
+  return '';
+};
+
+export const URL = 'http://localhost/rengasvarasto-back/API/';
+export const CUSTOMER = 'customer/';
+
+
+export default Config;
--- src/sivut/Asiakas.js
+++ src/sivut/Asiakas.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { useEffect, useState, alert } from 'react';
 import axios from 'axios';
-
-const URL = "http://localhost/rengashotelli-back/API/customer/read_single.php";
+import {URL,CUSTOMER} from '../back/Config';
 
 export default function Asiakas() {
   const[etunimi, setEtunimi] = useState("");
   const[sukunimi, setSukunimi] = useState("");
 
   useEffect(()=>{
-    axios.get(URL + "?id=1")
+    axios.get(URL + CUSTOMER + "read_single.php?id=1")
     .then((response) => {
       setEtunimi(response.data.etunimi);
       setSukunimi(response.data.sukunimi);
