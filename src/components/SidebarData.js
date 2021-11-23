bf7c9fe355c087ba280c2cd03f37caf20863dd6a
Maria Järvinen <n9jama00@students.oamk.fi>
2021-11-23 14:32:35 +0200

    navin järjestelyä

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -3,52 +3,50 @@ import {FaHome, FaWarehouse, FaSearch, FaQuestion } from 'react-icons/fa';
 import {MdPersonAdd, MdPerson } from 'react-icons/md';
 
 export const SidebarData = [
-  
     {
-     title: 'Etusivu',
-     path: '/home',
-     icon: <FaHome />,
-     cName: 'nav-text'
+        title: 'Etsi',
+        path: '/search',
+        icon: <FaSearch />,
+        cName: 'nav-text'
     },
 
+    {
+        title: 'Uusi asiakas',
+        path: '/newCustomer',
+        icon: <MdPersonAdd />,
+        cName: 'nav-text'
+    },
     {
         title: 'Asiakkaat',
         path: '/customers',
         icon: <MdPerson />,
         cName: 'nav-text'
-       },
+    },
 
-       {
-        title: 'Uusi asiakas',
-        path: '/newCustomer',
-        icon: <MdPersonAdd />,
-        cName: 'nav-text'
-       },
-
-       {
-        title: 'Etsi',
-        path: '/search',
-        icon: <FaSearch />,
-        cName: 'nav-text'
-       },
-
-       {
+    {
         title: 'Varasto',
         path: '/Warehouse',
         icon: <FaWarehouse />,
         cName: 'nav-text'
-       },
+    },
 
-       {
+    {
         title: 'Tyhjä',
         path: '/empty',
         icon: <FaQuestion />,
         cName: 'nav-text'
-       },
-       {
+    },
+
+    {
         title: 'Asiakas',
         path: '/Customer',
         icon: <MdPerson />,
         cName: 'nav-text'
-       },
+    },
+    {
+        title: 'Kirjaudu ulos',
+        path: '/empty',
+        icon: <FaQuestion />,
+        cName: 'nav-text'
+    }
 ]
