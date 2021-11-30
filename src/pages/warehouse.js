b16059b127474e1e409b5eaf2e15f5d652664a48
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2021-11-30 10:58:36 +0200

    Warehouse

--- src/pages/warehouse.js
+++ src/pages/warehouse.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
-
+import {FaTimes, FaHome, FaWarehouse, FaSearch, FaQuestion } from 'react-icons/fa';
 
 // tänne lista kaikista varastopaikoista lajiteltuna varastoittain
 
 export default function Warehouse({url}) {
-  const [warehouseAll, setWarehouseAll] = useState("0");
-  const [warehouseTaken, setWarehouseTaken] = useState("0");
-  const [warehouseFree, setWarehouseFree] = useState("0");
+  const [warehouseAll, setWarehouseAll] = useState(0);
+  const [warehouseTaken, setWarehouseTaken] = useState(0);
+  const [warehouseFree, setWarehouseFree] = useState(0);
+  const [degree, setDegree] = useState(0);
+  const [colorTaken, setColorTaken] = useState("#E9897E");
+  const [colorFree, setColorFree] = useState("#A0DAA9");
 
   useEffect(() => {
     async function getWarehouseData() {
@@ -14,9 +17,10 @@ export default function Warehouse({url}) {
           const response = await fetch(url + 'warehouse/warehouse_amounts.php');
             const json = await response.json();
             if (response){
-              setWarehouseAll(json.all);
-              setWarehouseTaken(json.taken);
-              setWarehouseFree(json.free);
+              setWarehouseAll(parseInt(json.all));
+              setWarehouseTaken(parseInt(json.taken));
+              setWarehouseFree(parseInt(json.free));
+              setDegree(parseInt(json.degree));
             } else {
                 alert(json.error);
             }
@@ -25,17 +29,46 @@ export default function Warehouse({url}) {
         }
     }
     getWarehouseData();
-}, []);
+  }, []);
 
+const PieChart = {
+  'background-image': 'conic-gradient(' + colorTaken + ' ' + degree + 'deg,' + colorFree + ' 0)'
+}
 
   return (
     <>
       <h1>Varastotilanne</h1>
-      <div>
-        <p>Kaikki paikat: {warehouseAll}</p>
-        <p>Varattu: {warehouseTaken}</p>
-        <p>Vapaana: {warehouseFree}</p>
-        <div id="warehouse-pie-chart" style={{'background-image': "conic-gradient(pink 70deg, lightblue 0 235deg, orange 0)"}}></div>
+      <div className="row">
+        <div className="row pie-back">
+          <div className="col">
+            <div className="text-center">
+            <h5>Paikkoja yhteensä</h5>
+            <h5 className="text-muted">{warehouseAll}</h5>
+            </div>
+            <div className="row">
+              <div className="col text-center">
+                <h5>Varattuna</h5>
+                <h5 className="text-muted">{warehouseTaken}</h5>
+              </div>
+              <div className="col text-center">
+                <h5>Vapaana</h5>
+                <h5 className="text-muted">{warehouseFree}</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+          <div className="row">
+            <div className="col-2">
+              <div id="warehouse-pie-chart" style={PieChart}></div>
+            </div>
+            <div className="col-3 pie-text">
+              <p><FaSearch />Varattuna</p>
+              <p><FaSearch />Vapaana</p>
+
+            </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
