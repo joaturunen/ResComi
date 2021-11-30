78022479accbca6e87898e0d4ff3ebee7e1bc614
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2021-11-30 12:14:23 +0200

    Värien uudelleen asetusksen aloittaminen

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import {FaWarehouse, FaSearch } from 'react-icons/fa';
 import {MdPersonAdd } from 'react-icons/md';
 import {GiTireIron} from 'react-icons/gi';
 import Print from '../printable/Print';
+import {boxShadowStyle} from '../style/colors';
 
 // tänne pitäs lisätä linkit noihin sivuihin samoin kuin navbarissa on
 
@@ -13,32 +14,32 @@ export default function Home() {
     <>
         <h1>DashBoard</h1>
       <div className='row marginHome'>
-        <button className='col-3 button'>
+        <button className='col-3 button' style={boxShadowStyle}>
           <FaSearch size={30}/>
           <p>Hae</p>
         </button>
-        <button class='col-3 button'>
+        <button class='col-3 button' style={boxShadowStyle}>
           <FaWarehouse size={40}/>
           <p>Varasto</p>
         </button>
         </div>
         <div className='row'>
-        <button class='col-3 button'>
+        <button class='col-3 button' style={boxShadowStyle}>
           <MdPersonAdd size={40} />
           <p>Uusi Asiakas</p>
         </button>
 
-        <button class='col-3 button'>
+        <button class='col-3 button' style={boxShadowStyle}>
           <GiTireIron size={40} />
           <p>Valmiit työt</p>
         </button>
 
-        <Link to="../printable/Print" class='col-3 button'>
+        <Link to="../printable/Print" class='col-3 button' style={boxShadowStyle}>
           <GiTireIron size={40} />
           <p>Printtisivutesti</p>
         </Link>
 
-        <Link to="./customer" class='col-3 button'>
+        <Link to="./customer" class='col-3 button' style={boxShadowStyle}>
          <MdPersonAdd size={40} /> 
           <p>Asiakas</p>
         </Link>    
--- src/style/colors.js
+++ src/style/colors.js
@@ -1,11 +1,24 @@
-import React, { useState, useEffect } from 'react';
 
+//Head color
 const headColor = "#0088c2";
-const headColorShavdow = "#006ea6";
+const headColorShadow = "#006ea6";
+
+//Box and Shadow
+const boxColor = "#e9eaeb";
+const boxShadow = "#D3D3D3";
+
+//Shock color
+
 
 //Header color
 export const headerColor = {
-  'background': 'linear-gradient(to bottom, ' + headColor + ' 55%,' + headColorShavdow  + ' 90%)'
+  'background': 'linear-gradient(to bottom, ' + headColor + ' 55%,' + headColorShadow  + ' 90%)'
+};
+
+//Grey box with shadow
+export const boxShadowStyle = {
+  'background': boxColor,
+  'box-shadow': '5px 5px ' + boxShadow
 };
 
 // Not sure why this must be
