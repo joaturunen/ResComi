7358e1a49ad37ed2404cfc716613bb8bb96ef4a5
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2021-11-30 11:43:25 +0200

    Väriasetukset samaan paikkaan

---
+++ src/style/colors.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+const headColor = "#0088c2";
+const headColorShavdow = "#006ea6";
+
+//Header color
+export const headerColor = {
+  'background': 'linear-gradient(to bottom, ' + headColor + ' 55%,' + headColorShavdow  + ' 90%)'
+};
+
+// Not sure why this must be
+const Colors = () =>{
+  return '';
+};
+export default Colors;
