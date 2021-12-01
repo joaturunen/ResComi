e83531fd766f0805d6a0a9994b085a6697c4041b
Hannes Sirviö <c9siha01@students.oamk.fi>
2021-12-01 10:33:37 +0200

    Asiakastabeja

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { MdEmojiObjects } from 'react-icons/md';
 import Tab from '../components/tab/Tab';
+<<<<<<< HEAD
+=======
 import CustomerInfo from './customerInfo';
 import Car from './car';
+>>>>>>> 99c44d0bd2b23df372762ab910736eb6bb8f7bde
 
 // tämä avautuu hakutuloksesta, ei näy navissa
 
@@ -11,11 +14,71 @@ export default function Customer({ url, customer, car }) {
   const tabContent = [
     {
       title: "Perustiedot",
+<<<<<<< HEAD
+      content:
+        <div class="row col-sm-6">
+          <div class="col-sm-4">
+            <label>Etunimi</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-sm-4">
+            <label>Sukunimi</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-sm-4">
+            <label>Asiakkuus luotu</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-sm-4">
+            <label>Lähiosoite</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-sm-4">
+            <label>Postinumero</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-sm-4">
+            <label>Postitoimipaikka</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-sm-4">
+            <label>Puhelinumero</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-sm-1">
+            <label class="form-check-label" for="suoramarkkinointi1">Lupa?</label>
+            <input type="checkbox" class="form-check-input" id="suoramarkkinointi1" value="phone" />
+          </div>
+          <div class="col-sm-4">
+            <label>Sähköposti</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-sm-1">
+            <label class="form-check-label" for="suoramarkkinointi2">Lupa?</label>
+            <input type="checkbox" class="form-check-input" id="suoramarkkinointi2" value="email" />
+          </div>
+          <div class="col-sm-12">
+            <label>Ajoneuvot</label>
+            <ul>
+              <li>LOL-666</li>
+              <li>LOL-666</li>
+              <li>LOL-666</li>
+            </ul>
+          </div>
+          <div class="col-sm-6">
+            <button class="btn btn-primary">Lisää ajoneuvo</button>
+          </div>
+          <div class="col-sm-6">
+            <button class="btn btn-primary">Tallenna</button>
+          </div>
+        </div>,
+=======
       content: <div class="col-sm-3">
         <label>Etunimi</label>
         <input type="text" class="form-control" />
         {/* <CustomerInfo customer={customer} /> */}
       </div>,
+>>>>>>> 99c44d0bd2b23df372762ab910736eb6bb8f7bde
     },
     {
       title: "Ajoneuvo",
@@ -24,7 +87,36 @@ export default function Customer({ url, customer, car }) {
       <div class="col-sm-4">
         <label>Rekisterinumero</label>
         <input type="text" class="form-control" />
+<<<<<<< HEAD
+      </div>
+      <div class="col-sm-4">
+        <label>Merkki</label>
+        <input type="text" class="form-control" />
+      </div>
+      <div class="col-sm-4">
+        <label>Malli</label>
+        <input type="text" class="form-control" />
+      </div>
+      <div class="col-sm-4">
+        <label>Vuosimalli</label>
+        <input type="text" class="form-control" />
+      </div>
+      <div class="col-sm-4">
+        <label>Säilytyskausi</label>
+        <div class="col-sm-6">
+          <select class="form-select">
+          <option value="1">Kesä</option>
+          <option value="2">Talvi</option>
+        </select>
+        </div>       
+      </div>
+      <div class="col-sm-4">
+        <label>Lisätietoja</label>
+        <textarea rows="3"/>
+      </div>
+=======
         {/* <Car car={car}/> */}
+>>>>>>> 99c44d0bd2b23df372762ab910736eb6bb8f7bde
       </div>,
     },
     {
