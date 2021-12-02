a89102b5bc700660f906e7a4ea244ff85e7aae41
Maria Järvinen <n9jama00@students.oamk.fi>
2021-12-02 11:37:41 +0200

    sidebar icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo3 from '../images/logo3.svg';
 import {headerColor} from '../style/colors';
+import {FaSignOutAlt } from 'react-icons/fa'; 
 
 export default function Header() {
   return (
     <div className='row header' style={headerColor}>
       <div className='col'>
         <a href='/'> <img className='logo' src={logo3} alt='rescomi'/> </a>
-        <p className='user'>Kirjautunut käyttäjä, Toimipiste</p>
+        <p className='user'>Kirjautunut käyttäjä, Toimipiste <FaSignOutAlt /></p>
       </div>
     </div>
   )
