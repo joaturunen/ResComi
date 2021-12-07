e0bbc7a25be796f26a8dcfc336386db158ecf45f
Maria Järvinen <n9jama00@students.oamk.fi>
2021-12-07 13:21:23 +0200

    car

--- src/pages/car.js
+++ src/pages/car.js
@@ -1,14 +1,43 @@
-import React , {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NewCar from './newCar';
 
 // tulostaa yhdelle asiakkaalle kuuluvat autot 
 
-export default function Car({customerCars, setCustomerCars, customer_id}) {
+export default function Car({ customerCars, setCustomerCars, customer_id }) {
 
     return (
         <div>
-            <div>
+            <div className='row'>
                 <h5>Auton tiedot</h5>
+                <div className="col-sm-4">
+                    <label>Rekisterinumero</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col-sm-4">
+                    <label>Merkki</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col-sm-4">
+                    <label>Malli</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col-sm-4">
+                    <label>Vuosimalli</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col-sm-4">
+                    <label>Säilytyskausi</label>
+                    <div className="col-sm-6">
+                        <select className="form-select">
+                            <option value="1">Kesä</option>
+                            <option value="2">Talvi</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <label>Lisätietoja</label>
+                    <textarea rows="3" />
+                </div>
                 <table className="table px-3 table-striped">
                     <tbody>
                         {customerCars.map(car => (
@@ -22,10 +51,10 @@ export default function Car({customerCars, setCustomerCars, customer_id}) {
                 </table>
             </div>
             <div>
-                <NewCar setCustomerCars={setCustomerCars} customer_id={customer_id}/>
+                <NewCar setCustomerCars={setCustomerCars} customer_id={customer_id} />
             </div>
-            
-            
+
+
         </div>
     );
 }
