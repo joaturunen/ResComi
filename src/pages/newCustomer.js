df30307801278c17868ddcb42737e33a889b9a8d
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2021-12-08 13:41:35 +0200

    Respo korjattu uusiAsiakas

--- src/pages/newCustomer.js
+++ src/pages/newCustomer.js
@@ -46,15 +46,14 @@ export default function NewCustomer({url}) {
   }
 
   return (
-      <div className='row'>
-          <div className=' searchCar' style={boxColorLayot}>
-        <h4> Uusi asiakas</h4>
-        <hr></hr>
+      <>
+        <div className='searchCar' style={boxColorLayot}>
+        <h4>Uusi asiakas</h4>
+        <hr/>
         <div className='d-flex justify-content-end'><p>Asiakkuus luotu:</p></div>
         <div>
           <form className='row' onSubmit={addCustomer}>
-           
-                <div className='col-md-3'>
+              <div className='col-md-3'>
               <div>
               <label>Etunimi</label>
                   <input type="text" className="form-control" value={firstname} onChange={e => setFirstname(e.target.value)}/>
@@ -79,36 +78,25 @@ export default function NewCustomer({url}) {
               
               
               <div className='col-md-3'>
-              <div>
-              <label>Postinumero</label>
-                  <input type="text" className="form-control" value={zipcode} onChange={e => setZipcode(e.target.value)}/>
+                <div>
+                <label>Postinumero</label>
+                    <input type="text" className="form-control" value={zipcode} onChange={e => setZipcode(e.target.value)}/>
+                </div>
+                <div>
+                <label>Postitoimipaikka</label>
+                    <input type="text" className="form-control" value={city} onChange={e => setCity(e.target.value)}/>
+                </div>
               </div>
-              <div>
-              <label>Postitoimipaikka</label>
-                  <input type="text" className="form-control" value={city} onChange={e => setCity(e.target.value)}/>
-              </div>
-              
-              <div className='row'>
-              <div>
-             <label>Suoramarkkinointilupa</label>
-             </div>
-             <div>
-             <input type="radio" name="marketing" value="phone" defaultChecked onChange={e => setMarketing(e.target.value)}/><label>Puhelin</label>
-             </div>
-             <div>
-             <input type="radio" name="marketing" value="mail" onChange={e => setMarketing(e.target.value)}/><label>Sähköposti</label>
-             </div>
-             </div>
-              </div>
+
               <div className='col-md-3'> <p>Ajoneuvot:</p></div>
               <div className='row'>
             <div className='col-md-12 d-flex justify-content-end '>
-            <button className='btn btn-primary' style={buttonStyle}>Tallenna</button>
+            <button className='btn' style={buttonStyle}>Tallenna</button>
             </div>
             </div>
           </form>
           </div>
           </div>
-      </div>
+      </>
   );
 }
