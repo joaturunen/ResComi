9b73c209b7b08d9bf07c1448b6e6c0c8c05a2056
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-12-08 09:18:20 +0200

    orderia

--- src/pages/order.js
+++ src/pages/order.js
@@ -22,37 +22,40 @@ export default function Order({
     customerCars, 
     setCustomerCars, 
     customerTires, 
-    setCustomerTires}) {
+    setCustomerTires,
+    employee_id}) {
     
     const [finished, setFinished] = useState(false);
+    const [cus_id, setCus_id] = useState(customer_id);
+    const [employ_id, setEmploy_id] = useState(employee_id);
 
-    // tämä ei vielä toimi, ei ole backissa!!
-    // function order(e) {
-    //     e.preventDefault();
-    //     fetch(url + 'order/add.php', { 
-    //         method: 'POST',
-    //         header: {
-    //             'Accept': 'application/json',
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({
-    //             customer: customer,
-    //             cart: cart,
-    //         })
-    //     })
-    //     .then (res => {
-    //         return res.json();
-    //     })
-    //     .then (
-    //         (res) => {
-    //             console.log(res);
-    //             empty();
-    //             setFinished(true);
-    //         }, (error) => {
-    //             alert(error);
-    //         }
-    //     );
-    // }
+    function SaveOrder(e) {
+        e.preventDefault();
+        fetch(url + 'order/order_create.php', { 
+            method: 'POST',
+            header: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                customer_id: cus_id,
+                employee_id: employ_id,
+                cart: cart,
+            })
+        })
+        .then (res => {
+            return res.json();
+        })
+        .then (
+            (res) => {
+                console.log(res);
+                empty();
+                setFinished(true);
+            }, (error) => {
+                alert(error);
+            }
+        );
+    }
 
 
     let sum = 0;
@@ -84,6 +87,10 @@ export default function Order({
                                 </tr>
                             </tbody>
                         </table>
+                        <form onSubmit={SaveOrder}>
+                            <input placeholder="asiakasnumero" value={cus_id} />
+                            <button class='btn btn-primary' style={buttonStyle}>Tallenna tilaus</button>
+                        </form>
                     </div>
                     <div className='col-1'></div>
                     <div className='col-4'>
