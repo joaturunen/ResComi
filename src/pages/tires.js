317bcae9354df699b0c53a165b1d400a4e3880ff
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-12-13 09:16:13 +0200

    delete tires

--- src/pages/tires.js
+++ src/pages/tires.js
@@ -3,8 +3,39 @@ import NewTires from "./newTires";
 import Warehouse from "./warehouse";
 import Car from '../images/3121893.png'
 import { buttonStyle } from "../style/colors";
+import { FaTrash } from 'react-icons/fa';
 
 export default function Tires({ customerTires, setCustomerTires, car_id }) {
+
+    function deleteTires(id) {
+        let status = 0;
+        fetch('http://localhost/rengasvarasto-back/API/tires/tires_delete.php', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: id
+            })
+        })
+        .then(res => {
+            status = parseInt(res.status);
+            return res.json();
+        })
+        .then(
+            (res) => {
+                if (status === 200) {
+                    const newListWithoutRemoved = customerTires.filter((tires) => tires.id !== id);
+                    setCustomerTires(newListWithoutRemoved);
+                  } else {
+                    alert(res.error);
+                  }
+            }, (error) => {
+                alert(error);
+            }
+        );
+    }
 
     return (
         <div>
@@ -112,6 +143,7 @@ export default function Tires({ customerTires, setCustomerTires, car_id }) {
                                 <td>{tires.rims}</td>
                                 <td>{tires.info}</td>
                                 <td>{tires.slot_id}-{tires.shelf_id}-{tires.warehouse_id}</td>
+                                <td><FaTrash onClick={() => deleteTires(tires.id)}/></td>
                             </tr>
                         ))}
                     </tbody>
