af657a22a6fe2477f2bb58560ec7edf0e89cc8f6
Maria Järvinen <n9jama00@students.oamk.fi>
2021-12-16 19:20:10 +0200

    header linkki

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,12 @@ import {FaSignOutAlt } from 'react-icons/fa';
 export default function Header() {
   return (
     <div className='row' style={headerColor}>
+      <div class='col-2'>
         <a href='/'> <img className='logo' src={logo3} alt='rescomi'/> </a>
-        
+        </div>
+        <div>
         <p style={userStyle}>Kirjautunut käyttäjä, Toimipiste <FaSignOutAlt /></p>
+        </div>
     </div>
   )
 }
